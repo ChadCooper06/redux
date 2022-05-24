0569314d500deba3ab17fea8a5708fdc0b3562aa
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-05-24 18:05:26 +0000

    Installed redux and React

---
+++ learn-redux/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          The new page will display here.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
